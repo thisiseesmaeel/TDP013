2ba0e447e03ab6841995dfe25d34577fe158cc72
Hadi <hadan326@student.liu.se>
2021-09-10 12:35:58 +0200

    Used bootstrap in lab1

--- lab1/javascript.js
+++ lab1/javascript.js
@@ -8,7 +8,7 @@ function post(msg)
     if(msg.length > 140 || msg.length == 0)
     {
         let errorBox = document.getElementById("error_div");
-        errorBox.textContent = "Error!";
+        errorBox.textContent = "You cannot write empty message or more than 140 characters!";
         return false;
     }
     let messageNode = document.createTextNode(msg);
@@ -36,6 +36,7 @@ function post(msg)
     //// Skapar message_main_box div
     let messageMainBox = document.createElement("div");
     messageMainBox.className += "message_main_box";
+    messageMainBox.setAttribute("id", "message_main_div" + msgCounter.toString());
     messageMainBox.appendChild(messageTextBox);
     messageMainBox.appendChild(messageStatusBox);
     ////
@@ -50,25 +51,15 @@ function post(msg)
 function init(){
     let searchField = document.getElementById("message_input");
     searchField.value = "";
-    //console.log(document.cookie);
-
-    //let mylist = [["hadi", 1], ["ali", 3], ["oskar", 2]];
-    //mylist.sort(function(a,b){return a[1] - b[1];}); // fallande sortering efter andra element
-    //mylist.sort(function(x,y){return x[1].localeCompare(y[1]);});
-    //console.log(mylist);
-    
     if(document.cookie)
     {
         let cookiesList = document.cookie.split(";");
         let sortedCookies = [];
-        console.log(cookiesList);
-
         for(let i = 1; i <= cookiesList.length; i++)
         {               
             let reg = cookiesList[i - 1].match(/(.*)=(.*)#(\d+)/);
             sortedCookies.push([reg[1], reg[2], reg[3]]);            
         }
-        console.log(sortedCookies);
         sortedCookies.sort(function(a,b){return a[2] - b[2];});
         for(let i = 1; i <= sortedCookies.length; i++)
         {
@@ -79,12 +70,9 @@ function init(){
                 changeStatus(i);
             }
         }
-        console.log(sortedCookies);
         msgCounter = cookiesList.length + 1;
- 
     }
         
-    
 
     document.getElementById("send_button").addEventListener("click", function (event){
 
@@ -98,7 +86,6 @@ function init(){
             errorBox.textContent = "";
             msgCounter += 1;
             statusListener();
-            console.log("hereeeeeeee: " + msgCounter.toString());
         }     
 
     })
@@ -110,7 +97,6 @@ function init(){
 
 function statusListener()
 {
-    console.log("msgCounter : " + msgCounter.toString());
     for (let i = 1; i < msgCounter; i++)
     {
         document.getElementById("status_button" + i.toString()).addEventListener("click", function (event){
@@ -128,10 +114,7 @@ function changeStatus(i)
     statusBtn.innerHTML = "read";
     let x = document.getElementById("message_text_div" + i.toString());
     document.cookie =  x.textContent + "=read" + "#" + i.toString();
-    console.log(document.cookie);
     statusBtn.style.color = "green";
-    let messageTextBox = document.getElementById("message_text_div" + i.toString());
-    messageTextBox.style.color = "gray";
-    let messageBox = document.getElementById("message_text_div" + i.toString());
-    messageBox.style.backgroundColor = "darkblue";
+    let messageMainBox = document.getElementById("message_main_div" + i.toString());
+    messageMainBox.style.backgroundColor = "rgb(86, 133, 89)";
 }
