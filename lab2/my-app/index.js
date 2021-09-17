a280eface24c77eacc4bbfeac8bb331272454b09
Hadi <hadan326@student.liu.se>
2021-09-17 09:50:52 +0200

    Routes fungerar (utan felhantering)

--- lab2/my-app/index.js
+++ lab2/my-app/index.js
@@ -9,57 +9,90 @@ const ObjectID = require('mongodb').ObjectID
 const { MongoClient } = require('mongodb')
 let url = "mongodb://localhost:27017/"
 
-
+// För testing
 app.get('/', function (req, res) {
-
-    MongoClient.connect(url, (err, db) => {
-        if(err) { throw err; }
-        let dbo = db.db("tdp013")
-    
-        dbo.collection("messages").find({}).toArray((err, result) => {
-            if(err){ throw err; }
-            console.log(result)
-            db.close()
-        })
-    });
-    res.send("Hello World!")
+    res.send("Welcome!")
 });
-    
+
+// Spara meddelande
 app.post('/save', function (req, res) {
     const { message, status } = req.body;
-    console.log(message)
-    console.log(status)
 
     MongoClient.connect(url, (err, dbs) => {
         if(err) { throw err; }
         let dbo = dbs.db("tdp013")
         dbo.collection("messages").insertOne({"message" : message, "status" : status})
+        .then((data) => {
+            dbs.close()
+        })
+        .catch((err) => {
+            console.log("Something went wrong..." + err)
+            dbs.close()
+        })
+        
     });
 
     res.send('Post fungerar!')
 });
 
-app.post('/update', function (req, res) {
+
+// Markera som läst/oläst
+app.post('/flag', function (req, res) {
     const { id, status } = req.body;
-    console.log(id)
-    console.log(status)
 
     MongoClient.connect(url, (err, dbs) => {
         if(err) { throw err; }
         let dbo = dbs.db("tdp013")
-
         let mongo = require('mongodb')
 
-
         dbo.collection("messages").updateOne({"_id": new mongo.ObjectId(id)}, {$set: {"status" : status}})
+        .then((data) => {
+            res.send('Update fungerar!')
+            dbs.close()
+        })
+        .catch((err) => {
+            console.log("Something went wrong..." + err)
+            dbs.close()
+        })
+        
     });
-
-    res.send('Update fungerar!')
+    
 });
 
+// Hämta meddelande
+app.get('/get', function (req, res) {
+    const id = req.query.id
 
-// //you can now query
-// Model.find({_id: good_id})
+    MongoClient.connect(url, (err, dbs) => {
+        if(err) { throw err; }
+        
+        let dbo = dbs.db("tdp013")
+        let mongo = require('mongodb')
+
+        dbo.collection("messages").find({"_id": new mongo.ObjectId(id)}).toArray((err, result) => {
+            if(err){ throw err; }
+            
+            res.send(result)
+            dbs.close()
+        })
+    });
+});
+
+// Hämta alla meddelande
+app.get('/getall', function (req, res) {
+
+    MongoClient.connect(url, (err, dbs) => {
+        if(err) { throw err; }
+        let dbo = dbs.db("tdp013")   
+        dbo.collection("messages").find({}).toArray((err, result) => {
+            if(err){ throw err; }
+            
+            res.send(result)
+            dbs.close()
+        })
+    });
+});
+
 
 
 
