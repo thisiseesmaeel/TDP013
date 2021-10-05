caa9d91d45e2f772a18e0a888fd5c224a02edf2a
Hadi <hadan326@student.liu.se>
2021-10-05 23:39:55 +0200

    fixat ett bug (man kan inte posta på andras sidor om man inte är vän med dem)

--- project/back-end/routes/write-post.js
+++ project/back-end/routes/write-post.js
@@ -6,10 +6,10 @@ const url = "mongodb://localhost:27017/";
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  const {myUsername, loggedInID, friendUsername, post} = req.body;
-  if(typeof(post) != "string")
+  const {myUsername, loggedInID, destUsername, post} = req.body;
+  if(typeof(myUsername) != "string" || typeof(loggedInID) != "number" || typeof(destUsername) != "string" || typeof(post) != "string")
   { 
-      res.status(400).send("Wrong parameter!");
+    res.status(400).send("Wrong parameter!");
   }
   else{
     MongoClient.connect(url, (error, database) => {
@@ -18,8 +18,11 @@ router.post('/', function(req, res, next) {
         // legtimera att allt stämmer med användaren
         dbo.collection("users").find({"username": myUsername}).toArray((error, result) => {
             if(error){ throw error; }
-            if(result[0].loggedInID == parseInt(loggedInID) && result[0].loggedInID != null){
-                dbo.collection("users").updateOne({"username": friendUsername}, { $push: {"posts": {"body": post, "owner": myUsername, "time": new Date()}}}
+            if(!result[0].friends.includes(destUsername) && result[0].username != destUsername){
+                res.status(404).send("Not found!");
+                database.close();    
+            }else if(result[0].loggedInID == parseInt(loggedInID) && result[0].loggedInID != null){
+                dbo.collection("users").updateOne({"username": destUsername}, { $push: {"posts": {"body": post, "owner": myUsername, "time": new Date()}}}
                   ,(error, result) => {
                     if(error){ throw error; }
                     if(result.length == 0){
