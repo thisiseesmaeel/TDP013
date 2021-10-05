31a511604031bfb419810cc13eaec41e627f4e33
Hadi <hadan326@student.liu.se>
2021-10-05 22:07:30 +0200

    find-user fungerar med login/out status

--- project/back-end/routes/find-user.js
+++ project/back-end/routes/find-user.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const url = "mongodb://localhost:27017/";
 
 router.get('/', function(req, res, next) {
-    const {me, user} = req.body;
+    const {myUsername, loggedInID, user} = req.body;
     const userArr = user.split(" ");
     firstname = userArr[0];
     lastname = userArr[1];
@@ -16,30 +16,43 @@ router.get('/', function(req, res, next) {
     MongoClient.connect(url, (error, database) => {
         if(error) { throw error; }
         let dbo = database.db("database");
-
-        dbo.collection("users").find({"firstname": firstname, "lastname": lastname}).toArray((error, searchResult) => {
+        dbo.collection("users").find({"username": myUsername}).toArray((error, result) => {
             if(error){ throw error; }
-            if(searchResult.length == 0){
-                res.status(204).send("No Content!");
-            }else{
-                let myFriends = [];
-                dbo.collection("users").find({"username": me}).toArray((error, result) => {
-                    if(error){ throw error; }
-                    if(result.length > 1){ res.status(500).send("Internal Server Error");}
-                    myFriends = result[0].friends;
-                    let foundUsers = [];
-                    searchResult.forEach((value) => {
-                        if(!myFriends.includes(value.username)){
-                            let tempUser = {"firstname": value.firstname ,"lastname": value.lastname, "username": value.username};
-                            foundUsers.push(tempUser);
+                if(result.length > 1){ res.status(500).send("Internal Server Error");}
+                if(result[0].loggedInID == parseInt(loggedInID) && result[0].loggedInID != null){
+                    let myFriends = result[0].friends;
+                    dbo.collection("users").find({"firstname": firstname, "lastname": lastname}).toArray((error, searchResult) => {
+                        if(error){ throw error; }
+                        if(searchResult.length == 0){
+                            res.status(204).send("No Content!");
+                        }else{
+                            let foundUsers = [];
+                            searchResult.forEach((value) => {
+                            if(!myFriends.includes(value.username) && value.username != myUsername){
+                                let tempUser = {"firstname": value.firstname ,"lastname": value.lastname, "username": value.username};
+                                foundUsers.push(tempUser);
+                            }
+                            });
+                            if(foundUsers.length <= 0){
+                                res.status(204).send("No Content!");
+                                database.close();
+                            }
+                            else{
+                                res.status(200).send(foundUsers);
+                                database.close();
+                            }
+                            
                         }
-
-                        });
-                    res.status(200).send(foundUsers);
+                        
+                    });
+                }
+                else{
+                    res.status(401).send("Unauthorized!");
                     database.close();
-                });
-            }
+                }
+                
         });
+
     });
 });
 
