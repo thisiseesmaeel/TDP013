c3d739e1effac52a7b71bdf7d04a34ea4ce1ab21
Ismail Safwat <ismail@hey.com>
2021-10-18 16:25:02 +0200

    Klar med komplettering (lab3).

---
+++ lab3/README.md
@@ -0,0 +1,10 @@
+## How to get started with the project (Linux ubuntu)
+After cloning repository follow these steps:
+
+1. Make sure you have installed **mongo** and **nodejs** in your system. You can check it by typing **_"mongo -version"_** and **_"node -v"_** in your command line. It is recommended to have at least **nodejs** version 14 or higher.
+1. In order to restore the database files, type **_"mongorestore --db = [database name] [database directory path]"_**. You can find the database directory in _"/lab2/database"_ directory.
+1. After opening the lab3, type **_"npm install jquery"_** to install the **jQuery** for the program, thereafter type **_"npm audit fix"_** to fix the installed modules. 
+1. Navigate to the back-end directory "cd **_lab2/my-app"_**.
+1. Now you can start the server (port 3000) by typing **_"npm start"_** in the terminal.
+1. Navigate back to the lab3 directory and open **_"main.html"_** file in your browser to see the result.
+1. **Enjoy!**
--- lab3/javascript.js
+++ lab3/javascript.js
@@ -99,33 +99,4 @@ $(function(){
     }
 let counter = 0;
 
-
-// $("#send_button").on("click", function(e){
-//     e.preventDefault();
-//     let msg = $("#message_input").val();
-//     $.post("http://localhost:3000/save",
-//     {
-//         message : msg,
-//         status: "unread"
-//     },
-//     function(data, status){
-//         if(status = "success")
-//         {
-//             let id = data;
-//             let $messageMainBox = $("<div>", {"class": "message_main_box", "id": id});
-//             let $messageTextBox = $("<div>", {"class": "message_text_box"});
-//             let $messageStatusBox = $("<div>", {"class": "message_status_box"});
-//             let $statusBtn = $("<button>", {"id": "btn" + id});
-//             $statusBtn.text("unread");
-
-//             $("#main_box>div:nth-child(2)").after($messageMainBox);
-//             $("#" + id).append($messageTextBox);
-//             $("#" + id).append($messageStatusBox);
-//             $messageStatusBox.append($statusBtn);
-            
-//             $messageTextBox.text(msg);
-//         }
-//         console.log(status);
-//     });
-//   });
 });
